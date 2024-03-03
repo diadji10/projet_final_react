8c6a1ded5c8b09f3e2cbf31548fda18d98898a69
diadji10 <diadjighost@gmail.com>
2024-03-03 19:37:48 +0000

    phase_test

--- src/Component/search/Search.js
+++ src/Component/search/Search.js
@@ -1,8 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Search.css";
+import axios from "axios";
 
 const Search = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+  const [filter, setFilter] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8ee9a3a7997ee232cdd6672a55453498"
+      )
+      .then((res) => console.log(res.data));
+  }, []);
+  return (
+    <>
+      <div className="search">
+        <input
+          type="search"
+          onChange={(e) => setFilter(e.target.value)}
+          required
+          placeholder="Search"
+        />
+        {/* <ul>
+          {/* {data
+            .filter((country) => {
+              if (filter === "") {
+                return "";
+              } else if (country.name.common.toLowerCase().includes(filter)) {
+                return country;
+              }
+            })
+            .map((el) => (
+              <li>{el.name.common}</li>
+            ))} 
+        </ul> */}
+      </div>
+    </>
+  );
 };
-
 export default Search;
