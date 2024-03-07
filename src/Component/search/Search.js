04e8a465784ad40277332410b38c816624931bf4
diadji10 <diadjighost@gmail.com>
2024-03-07 07:50:41 +0000

    test2

--- src/Component/search/Search.js
+++ src/Component/search/Search.js
@@ -5,35 +5,29 @@ import axios from "axios";
 const Search = () => {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState("");
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8ee9a3a7997ee232cdd6672a55453498"
-      )
-      .then((res) => console.log(res.data));
-  }, []);
+  const handleSubmit = () => {
+    useEffect(() => {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${filter}&appid=c33f32a7e6f2c88b9953d23d00881a8e`
+        )
+        .then((res) => console.log(res.data));
+    }, []);
+  };
   return (
     <>
       <div className="search">
-        <input
-          type="search"
-          onChange={(e) => setFilter(e.target.value)}
-          required
-          placeholder="Search"
-        />
-        {/* <ul>
-          {/* {data
-            .filter((country) => {
-              if (filter === "") {
-                return "";
-              } else if (country.name.common.toLowerCase().includes(filter)) {
-                return country;
-              }
-            })
-            .map((el) => (
-              <li>{el.name.common}</li>
-            ))} 
-        </ul> */}
+        <form onSubmit={() => handleSubmit()}>
+          <input
+            type="search"
+            onChange={(e) => setFilter(e.target.value)}
+            required
+            placeholder="Entrez le nom d'une ville"
+          />
+          <button type="submit">
+            <i className="fa-brands fa-searchengin"></i>
+          </button>
+        </form>
       </div>
     </>
   );
