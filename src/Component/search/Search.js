ca09f9edf96726f8c558dcdbd90f24686ca65721
diadji10 <diadjighost@gmail.com>
2024-03-10 19:46:59 +0000

    phase finale

--- src/Component/search/Search.js
+++ src/Component/search/Search.js
@@ -1,35 +1,60 @@
-import React, { useEffect, useState } from "react";
 import "./Search.css";
 import axios from "axios";
+import { useState } from "react";
 
 const Search = () => {
-  const [data, setData] = useState([]);
-  const [filter, setFilter] = useState("");
-  const handleSubmit = () => {
-    useEffect(() => {
-      axios
-        .get(
-          `https://api.openweathermap.org/data/2.5/weather?q=${filter}&appid=c33f32a7e6f2c88b9953d23d00881a8e`
-        )
-        .then((res) => console.log(res.data));
-    }, []);
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c33f32a7e6f2c88b9953d23d00881a8e`
+      );
+      console.log(response.data);
+      setWeatherData(response.data);
+      setLoading(false);
+    } catch (error) {
+      setError("Une erreur s'est produite. Veuillez réessayer.");
+      setLoading(false);
+    }
   };
   return (
-    <>
-      <div className="search">
-        <form onSubmit={() => handleSubmit()}>
-          <input
-            type="search"
-            onChange={(e) => setFilter(e.target.value)}
-            required
-            placeholder="Entrez le nom d'une ville"
+    <div className="search">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          required
+          placeholder="Entrez le nom d'une ville"
+        />
+        <button type="submit" disabled={loading}>
+          Rechercher
+        </button>
+      </form>
+      {loading && <p>Chargement...</p>}
+      {error && <p>{error}</p>}
+      {weatherData && (
+        <div className="weather-info">
+          <h2>{weatherData.name}</h2>
+          <img
+            alt=""
+            src={`./${weatherData.weather[0].main.toLowerCase()}.png`}
           />
-          <button type="submit">
-            <i className="fa-brands fa-searchengin"></i>
-          </button>
-        </form>
-      </div>
-    </>
+          <p>
+            Température: {Number.parseInt(weatherData.main.temp - 273.15)}°C
+          </p>
+          <p>Humidité: {weatherData.main.humidity}%</p>
+          <p>Pression: {weatherData.main.pressure} hPa</p>
+          <p>Vitesse du vent: {weatherData.wind.speed} m/s</p>
+        </div>
+      )}
+    </div>
   );
 };
 export default Search;
